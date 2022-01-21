f891e03c3892aedc46562b457f1f30efc9354e4c
leandrole8 <leandro_le8@hotmail.com>
2022-01-21 11:06:29 -0300

    criado metodo para adicionar objetos 'cozinha'

---
+++ src/main/java/com/leandroreis/algafood/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,30 @@
+package com.leandroreis.algafood.jpa;
+
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.leandroreis.algafood.AlgafoodApiApplication;
+import com.leandroreis.algafood.domain.model.Cozinha;
+
+public class ConsultaCozinhaMain {
+	
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		List<Cozinha> cozinhas = cadastroCozinha.listar();
+		
+		for(Cozinha cozinha : cozinhas) {
+			System.out.println(cozinha.getNome());
+		}
+		
+	}
+
+}
--- src/main/java/com/leandroreis/algafood/jpa/InclusaoCozinhaMain.java
+++ src/main/java/com/leandroreis/algafood/jpa/InclusaoCozinhaMain.java
@@ -1,8 +1,6 @@
 package com.leandroreis.algafood.jpa;
 
 
-import java.util.List;
-
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -10,7 +8,7 @@ import org.springframework.context.ApplicationContext;
 import com.leandroreis.algafood.AlgafoodApiApplication;
 import com.leandroreis.algafood.domain.model.Cozinha;
 
-public class CozinhaConsultaMain {
+public class InclusaoCozinhaMain {
 	
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
@@ -19,11 +17,18 @@ public class CozinhaConsultaMain {
 		
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 		
-		List<Cozinha> cozinhas = cadastroCozinha.listar();
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Brasileira");
 		
-		for(Cozinha cozinha : cozinhas) {
-			System.out.println(cozinha.getNome());
-		}
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Japonesa");
+		
+		cozinha1 = cadastroCozinha.adicionar(cozinha1);
+		cozinha2 = cadastroCozinha.adicionar(cozinha2);
+		
+		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
+		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+		
 		
 	}
 
