e4bbbb1a67424009929f73d191912462f2bd5cea
leandrole8 <leandro_le8@hotmail.com>
2022-01-25 13:23:25 -0300

    adicionado metodo buscar por id nos controllers Cozinha e Estado

--- src/main/java/com/leandroreis/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/leandroreis/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,9 @@ package com.leandroreis.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +13,20 @@ import com.leandroreis.algafood.domain.model.Cozinha;
 import com.leandroreis.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)//a resposta vem em formato json
 public class CozinhaController {
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-	@GetMapping
+	@GetMapping()
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
+	}
+	
+	@GetMapping("/{id}")
+	public Cozinha buscar(@PathVariable Long id) {
+		return cozinhaRepository.buscar(id);
 	}
 
 }
