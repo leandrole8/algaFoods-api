75cdf58b3d130c761c75c2c6e6708eda67897cdd
leandrole8 <leandro_le8@hotmail.com>
2022-01-26 15:59:23 -0300

    Implementado metodo Put em Cozinha e Estado

--- src/main/java/com/leandroreis/algafood/api/controller/EstadoController.java
+++ src/main/java/com/leandroreis/algafood/api/controller/EstadoController.java
@@ -2,12 +2,15 @@ package com.leandroreis.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +42,21 @@ public class EstadoController {
 	
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
-	public Estado adicionar(Estado estado) {
+	public Estado adicionar(@RequestBody Estado estado) {
 		return estadoRepository.salvar(estado);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Estado> atualizar(@PathVariable Long id, @RequestBody Estado estado){
+		Estado estadoAtual = estadoRepository.buscar(id);
+		
+		if(estadoAtual != null) {
+			BeanUtils.copyProperties(estado, estadoAtual, "id");
+			estadoAtual = estadoRepository.salvar(estadoAtual);
+			
+			return ResponseEntity.ok(estadoAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
