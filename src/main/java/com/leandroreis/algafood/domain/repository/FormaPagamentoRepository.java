994df5e89337ff351cd59ca9b9c4c9e6df089b6f
leandrole8 <leandro_le8@hotmail.com>
2022-02-15 08:24:28 -0300

    Realizado ajustes na classe FormaPagamento

--- src/main/java/com/leandroreis/algafood/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/leandroreis/algafood/domain/repository/FormaPagamentoRepository.java
@@ -2,16 +2,12 @@ package com.leandroreis.algafood.domain.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.leandroreis.algafood.domain.model.FormaPagamento;
 
 @Repository
-public interface FormaPagamentoRepository {
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
 
-	List<FormaPagamento> listar();
-	FormaPagamento buscar(Long id);
-	FormaPagamento salvar(FormaPagamento formaPagamento);
-	void remover(FormaPagamento formaPagamento);
-	
 }
